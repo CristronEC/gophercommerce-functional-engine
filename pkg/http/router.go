60c648875edecead6bd080003252d595e2b69eb8
CristronEC <167824555+CristronEC@users.noreply.github.com>
2026-02-17 19:47:39 -0500

    Update router.go

---
+++ pkg/http/router.go
@@ -0,0 +1,16 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter(handler *Handler) http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/health", handler.HealthCheck)
+	r.Get("/products", handler.GetProducts)
+
+	return r
+}
